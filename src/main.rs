d7cc1b0f0b229bbdfcac8eb7c1eb3b4abe0f2702
mmkaram <mahdykaram@outlook.com>
2024-02-11 17:53:02 -0800

    got collisions to work
    on_ground func
    collider_check func
    has_lost func

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::{prelude::*, render::color, transform};
+
+const GRAVITY: f32 = 300.0;
 
 fn main() {
     App::new()
@@ -7,7 +9,7 @@ fn main() {
                 .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Test Game".into(),
+                        title: "Flappy Bird".into(),
                         resolution: (640.0, 480.0).into(),
                         resizable: false,
                         ..default()
@@ -17,56 +19,136 @@ fn main() {
                 .build(),
         )
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, has_lost)
         .add_systems(Update, character_movement)
         .run();
 }
 
 #[derive(Component)]
+pub struct Collider {}
+
+#[derive(Component)]
+pub struct Ground {}
+
+#[derive(Component)]
 pub struct Player {
     speed: f32,
+    jump_index: u8,
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-	let mut camera = Camera2dBundle::default();
+    // setup camera
+    let camera = Camera2dBundle::default();
+    commands.spawn(camera);
 
-    camera.projection.scaling_mode = ScalingMode::AutoMin {
-        min_width: 256.0,
-        min_height: 144.0,
-    };
-
-	commands.spawn(camera);
-
-    let texture = asset_server.load("bird.png");
-
+    // setup ground
     commands.spawn((
         SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.0, 0.0, 0.0),
+                custom_size: Some(Vec2::new(480.0, 10.0)),
+                ..default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, -240.0, 0.0),
+                ..default()
+            },
+            ..default()
+        },
+        Ground {},
+    ));
+
+	//setup colliders
+	commands.spawn((
+		SpriteBundle {
+			sprite: Sprite {
+				color: Color::rgb(0.0, 0.0, 0.0),
+				custom_size: Some(Vec2::new(10.0, 480.0)),
+				..default()
+			},
+			transform: Transform {
+				translation: Vec3::new(-320.0, 0.0, 0.0),
+				..default()
+			},
+			..default()
+		},
+		Collider {},
+	));
+
+    // setup player
+    let texture = asset_server.load("bird.png");
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(100.0, 100.0)),
+                ..default()
+            },
             texture,
             ..default()
         },
-        Player { speed: 100.0 },
+        Player {
+            speed: 225.0,
+            jump_index: 0,
+        },
     ));
 }
 
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Player)>,
+    mut characters: Query<(&mut Transform, &mut Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut transform, player) in &mut characters {
+    for (mut transform, mut player) in &mut characters {
         let movement_amount = player.speed * time.delta_seconds();
-		// if two keys are pressed, the character will move diagonally
-		// TODO: moving diagonally doubles the speed
-        if input.pressed(KeyCode::W) {
+        let gravity = GRAVITY * time.delta_seconds();
+        if player.jump_index > 0 {
             transform.translation.y += movement_amount;
+            player.jump_index += 1;
+        } else if input.pressed(KeyCode::Space) {
+            transform.translation.y += movement_amount;
+            player.jump_index += 1;
         }
-        if input.pressed(KeyCode::S) {
-            transform.translation.y -= movement_amount;
+        if player.jump_index >= 40 {
+            player.jump_index = 0;
         }
-        if input.pressed(KeyCode::D) {
-            transform.translation.x += movement_amount;
-        }
-        if input.pressed(KeyCode::A) {
-            transform.translation.x -= movement_amount;
+        if player.jump_index == 0 {
+            transform.translation.y -= gravity;
         }
     }
+}
+
+fn has_lost(
+	player: Query<(&Transform, &Player)>,
+	colliders: Query<(&Collider, &Transform, &Sprite)>,
+) {
+    for (transform, _) in &player {
+        // Check if the player has hit the ground
+        if transform.translation.y < -240.0 {
+			println!("Game Over")
+        }
+	}
+	if collider_checks(colliders, player) {
+		println!("Game Over 2")
+	}
+}
+
+fn collider_checks (
+    mut colliders: Query<(&Collider, &Transform, &Sprite)>,
+    player: Query<(&Transform, &Player)>,
+) -> bool {
+    for (_, transform, sprite) in colliders.iter_mut() {
+        let pos = transform.translation;
+		let size = sprite.custom_size.unwrap();
+		for (player_transform, _) in &player {
+			let player_pos = player_transform.translation;
+			if player_pos.x < pos.x + size.x / 2.0
+				&& player_pos.x > pos.x - size.x / 2.0
+				&& player_pos.y < pos.y + size.y / 2.0
+				&& player_pos.y > pos.y - size.y / 2.0
+			{
+				return true;
+			}
+		}
+    }
+	false
 }
